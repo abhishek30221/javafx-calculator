da7ffa030cddd13f12b87d21048aafcda1107236
Abhishek Gusain <abhishekgusain2705@gmail.com>
2021-01-22 09:26:20 +0530

    initial project files

---
+++ Calculator/src/application/MainController.java
@@ -0,0 +1,12 @@
+package application;
+
+import javafx.event.ActionEvent;
+
+public class MainController {
+	public void processNumber(ActionEvent event) {
+		
+	}
+	public void processOperator(ActionEvent event) {
+		
+	}
+}
---
+++ Calculator/src/application/Model.java
@@ -0,0 +1,8 @@
+package application;
+
+public class Model {
+	public float calculator(long number1 , long number2 , String operator) { 
+		return 0;
+		
+	}
+}
